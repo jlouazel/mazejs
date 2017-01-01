65c2363e0a24e2835f40d0ab27a53e406fb70b37
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2017-01-01 22:03:47 +0100

    Genrerate field with square objects

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
+const webpack = require('webpack');
+
 module.exports = {
-    context: __dirname + '/lib',
-    entry: './index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'maze.js'
-    },
-    module: {
-        rules: [{
-            test: /.js$/,
-            use: [{
-                loader: 'babel-loader'
-            }]
-        }]
-    }
+  context: `${__dirname}/lib`,
+  entry: './index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'maze.js',
+
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      use: [{
+        loader: 'babel-loader'
+      }]
+    }]
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ]
 };
